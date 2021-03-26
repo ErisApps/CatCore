b480644ade9554454e9c48d8d294a879a8348d8f
Eris <erisapps@outlook.be>
2021-03-26 01:32:00 +0100

    Added BS test mod project

---
+++ CatCoreTesterMod/Services/CatCoreTesterService.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using CatCore;
+using SiraUtil.Tools;
+using SiraUtil.Zenject;
+using Zenject;
+
+namespace CatCoreTesterMod.Services
+{
+	internal class CatCoreTesterService : IInitializable
+	{
+		private readonly SiraLog _logger;
+		private readonly ChatCoreInstance _chatCoreInstance;
+
+		public CatCoreTesterService(SiraLog logger, UBinder<Plugin, ChatCoreInstance> chatCoreInstance)
+		{
+			_logger = logger;
+			_chatCoreInstance = chatCoreInstance.Value;
+		}
+
+		public async void Initialize()
+		{
+			var userInfo = await _chatCoreInstance.TwitchService.HelixApiService.FetchUserInfo(loginNames: "realeris").ConfigureAwait(false);
+			if (userInfo != null)
+			{
+				var erisProfileData = userInfo.Value.Data.First();
+				_logger.Info($"Successfully requested info for user {erisProfileData.DisplayName}");
+			}
+			else
+			{
+				_logger.Warning("Something went wrong while trying to fetch the info of the requested user.");
+			}
+
+		}
+	}
+}
