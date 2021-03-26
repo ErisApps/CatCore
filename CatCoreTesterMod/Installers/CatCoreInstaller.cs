5f1532d01df093cb041815ddf75a839cd5f147e4
Eris <erisapps@outlook.be>
2021-03-26 01:32:00 +0100

    Added BS test mod project

---
+++ CatCoreTesterMod/Installers/CatCoreInstaller.cs
@@ -0,0 +1,28 @@
+﻿using CatCore;
+using CatCoreTesterMod.Services;
+using IPA.Logging;
+using SiraUtil;
+using SiraUtil.Zenject;
+using Zenject;
+
+namespace CatCoreTesterMod.Installers
+{
+	internal class CatCoreInstaller : Installer<Logger, ChatCoreInstance, CatCoreInstaller>
+	{
+		private readonly Logger _logger;
+		private readonly ChatCoreInstance _chatCoreInstance;
+
+		internal CatCoreInstaller(Logger logger, ChatCoreInstance chatCoreInstance)
+		{
+			_logger = logger;
+			_chatCoreInstance = chatCoreInstance;
+		}
+
+		public override void InstallBindings()
+		{
+			Container.BindLoggerAsSiraLogger(_logger);
+			Container.BindInstance(new UBinder<Plugin, ChatCoreInstance>(_chatCoreInstance)).AsSingle();
+			Container.BindInterfacesAndSelfTo<CatCoreTesterService>().AsSingle();
+		}
+	}
+}
---
+++ CatCoreTesterMod/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using CatCore;
+using CatCore.Logging;
+using CatCoreTesterMod.Installers;
+using IPA;
+using IPA.Logging;
+using SiraUtil.Zenject;
+using Zenject;
+
+namespace CatCoreTesterMod
+{
+	[Plugin(RuntimeOptions.DynamicInit)]
+	internal class Plugin
+	{
+		[Init]
+		public Plugin(Logger logger, Zenjector zenjector)
+		{
+			zenjector.OnApp<CatCoreInstaller>().WithParameters(logger, ChatCoreInstance.CreateInstance((level, context, message) => logger.GetChildLogger("CatCore").Log(level switch
+			{
+				CustomLogLevel.Trace => Logger.Level.Trace,
+				CustomLogLevel.Debug => Logger.Level.Debug,
+				CustomLogLevel.Information => Logger.Level.Info,
+				CustomLogLevel.Warning => Logger.Level.Warning,
+				CustomLogLevel.Error => Logger.Level.Error,
+				CustomLogLevel.Critical => Logger.Level.Critical,
+				_ => Logger.Level.Debug
+			}, $"{context} | {message}")));
+		}
+
+		[OnEnable, OnDisable]
+		public void OnState()
+		{
+			/* Plugin State Changed */
+		}
+	}
+}
