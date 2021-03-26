071efc2d4dd5a5d4aba80f6f0220915742cd336a
Eris <erisapps@outlook.be>
2021-03-26 01:28:26 +0100

    Added primitive TwitchService

---
+++ CatCore/Services/Twitch/TwitchService.cs
@@ -0,0 +1,19 @@
+﻿using CatCore.Services.Twitch.Interfaces;
+using Serilog;
+
+namespace CatCore.Services.Twitch
+{
+	public class TwitchService
+	{
+		private readonly ILogger _logger;
+		private readonly ITwitchHelixApiService _twitchHelixApiService;
+
+		internal TwitchService(ILogger logger, ITwitchHelixApiService twitchHelixApiService)
+		{
+			_logger = logger;
+			_twitchHelixApiService = twitchHelixApiService;
+		}
+
+		public ITwitchHelixApiService HelixApiService => _twitchHelixApiService;
+	}
+}
