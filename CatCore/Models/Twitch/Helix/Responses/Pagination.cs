f730b6c47b929fd5cd866fb31a3e1eadb97a3ec9
Eris <erisapps@outlook.be>
2021-03-28 22:17:42 +0200

    Added Search Channels endpoint to Twitch Helix service

---
+++ CatCore/Models/Twitch/Helix/Responses/Pagination.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatCore.Models.Twitch.Helix.Responses
+{
+	public readonly struct Pagination
+	{
+		[JsonPropertyName("cursor")]
+		public string Cursor { get; }
+
+		[JsonConstructor]
+		public Pagination(string cursor)
+		{
+			Cursor = cursor;
+		}
+	}
+}
---
+++ CatCore/Models/Twitch/Helix/Responses/ResponseBaseWithPagination.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace CatCore.Models.Twitch.Helix.Responses
+{
+	public readonly struct ResponseBaseWithPagination<T>
+	{
+		[JsonPropertyName("data")]
+		public List<T> Data { get; }
+
+		[JsonPropertyName("pagination")]
+		public Pagination Pagination { get; }
+
+		[JsonConstructor]
+		public ResponseBaseWithPagination(List<T> data, Pagination pagination)
+		{
+			Data = data;
+			Pagination = pagination;
+		}
+	}
+}
