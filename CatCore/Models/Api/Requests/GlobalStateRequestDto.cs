dbf8950a4d6ce02bc820978a834c1eca03b0c161
Eris <erisapps@outlook.be>
2021-04-03 15:42:15 +0200

    Added endpoint to store new global settings state

---
+++ CatCore/Models/Api/Requests/GlobalStateRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatCore.Models.Api.Requests
+{
+	internal readonly struct GlobalStateRequestDto
+	{
+		public bool LaunchWebAppOnStart { get; }
+		public bool ParseEmojis { get; }
+
+		[JsonConstructor]
+		public GlobalStateRequestDto(bool launchWebAppOnStart, bool parseEmojis)
+		{
+			LaunchWebAppOnStart = launchWebAppOnStart;
+			ParseEmojis = parseEmojis;
+		}
+	}
+}
