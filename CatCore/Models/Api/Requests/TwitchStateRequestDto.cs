6682854d245d92f7495d1ce375deabb7424db493
Eris <erisapps@outlook.be>
2021-04-03 16:44:39 +0200

    Added endpoint to store the new Twitch settings state

---
+++ CatCore/Models/Api/Requests/TwitchStateRequestDto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace CatCore.Models.Api.Requests
+{
+	internal readonly struct TwitchStateRequestDto
+	{
+		public bool SelfEnabled { get; }
+		/// <remark>
+		/// Key being the actual userId and the value being the loginname
+		/// </remark>
+		public Dictionary<string, string> AdditionalChannelsData { get; }
+		public bool ParseBttvEmotes { get; }
+		public bool ParseFfzEmotes { get; }
+		public bool ParseTwitchEmotes { get; }
+		public bool ParseCheermotes { get; }
+
+		[JsonConstructor]
+		public TwitchStateRequestDto(bool selfEnabled, Dictionary<string, string> additionalChannelsData, bool parseBttvEmotes, bool parseFfzEmotes, bool parseTwitchEmotes, bool parseCheermotes)
+		{
+			SelfEnabled = selfEnabled;
+			AdditionalChannelsData = additionalChannelsData;
+			ParseBttvEmotes = parseBttvEmotes;
+			ParseFfzEmotes = parseFfzEmotes;
+			ParseTwitchEmotes = parseTwitchEmotes;
+			ParseCheermotes = parseCheermotes;
+		}
+	}
+}
