33a0ed905de25a3d871871293df4efee271ca66f
Eris <erisapps@outlook.be>
2021-04-03 16:16:42 +0200

    Added a Twitch channel management service
    This service can be used to aggregate the user's own channel with the additional channels and provide enriched channel data

---
+++ CatCore/Services/Twitch/Interfaces/ITwitchChannelManagementService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CatCore.Models.Twitch.Helix.Responses;
+
+namespace CatCore.Services.Twitch.Interfaces
+{
+	public interface ITwitchChannelManagementService
+	{
+		List<string> GetAllActiveLoginNames(bool includeSelfRegardlessOfState);
+		Task<List<UserData>> GetAllChannelsEnriched();
+	}
+}
---
+++ CatCore/Services/Twitch/TwitchChannelManagementService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CatCore.Models.Twitch.Helix.Responses;
+using CatCore.Services.Interfaces;
+using CatCore.Services.Twitch.Interfaces;
+
+namespace CatCore.Services.Twitch
+{
+	public class TwitchChannelManagementService : ITwitchChannelManagementService
+	{
+		private readonly IKittenSettingsService _kittenSettingsService;
+		private readonly ITwitchAuthService _twitchAuthService;
+		private readonly ITwitchHelixApiService _twitchHelixApiService;
+
+		internal TwitchChannelManagementService(IKittenSettingsService kittenSettingsService, ITwitchAuthService twitchAuthService, ITwitchHelixApiService twitchHelixApiService)
+		{
+			_kittenSettingsService = kittenSettingsService;
+			_twitchAuthService = twitchAuthService;
+			_twitchHelixApiService = twitchHelixApiService;
+		}
+
+		public List<string> GetAllActiveLoginNames(bool includeSelfRegardlessOfState)
+		{
+			var allChannels = new List<string>();
+			var self = _twitchAuthService.LoggedInUser;
+			if (self != null && (_kittenSettingsService.Config.TwitchConfig.OwnChannelEnabled || includeSelfRegardlessOfState))
+			{
+				allChannels.Add(self.Value.LoginName);
+			}
+
+			allChannels.AddRange(_kittenSettingsService.Config.TwitchConfig.AdditionalChannelsData.Values);
+
+			return allChannels;
+		}
+
+		public async Task<List<UserData>> GetAllChannelsEnriched()
+		{
+			var allLoginNames = GetAllActiveLoginNames(true);
+			var userInfos = await _twitchHelixApiService.FetchUserInfo(loginNames: allLoginNames.ToArray()).ConfigureAwait(false);
+			return userInfos?.Data ?? new List<UserData>();
+		}
+	}
+}
