1d885f95a5872721da7843572990177365825e2e
Eris <erisapps@outlook.be>
2021-04-03 16:14:13 +0200

    Added Twitch config

--- CatCore/Models/Config/TwitchConfig.cs
+++ CatCore/Models/Config/TwitchConfig.cs
@@ -1,6 +1,16 @@
-﻿namespace CatCore.Models.Config
+﻿using System.Collections.Generic;
+
+namespace CatCore.Models.Config
 {
 	internal class TwitchConfig
 	{
+		public bool OwnChannelEnabled { get; set; } = true;
+
+		public Dictionary<string, string> AdditionalChannelsData { get; set; } = new Dictionary<string, string>();
+
+		public bool ParseBttvEmotes { get; set; } = true;
+		public bool ParseFfzEmotes { get; set; } = true;
+		public bool ParseTwitchEmotes { get; set; } = true;
+		public bool ParseCheermotes { get; set; } = true;
 	}
 }
