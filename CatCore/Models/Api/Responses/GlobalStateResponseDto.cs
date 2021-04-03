642f1bcc94e42262a1df77716dfd3ffdef997a52
Eris <erisapps@outlook.be>
2021-04-03 15:41:47 +0200

    Added endpoint to request global current settings state

---
+++ CatCore/Models/Api/Responses/GlobalStateResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using CatCore.Models.Config;
+
+namespace CatCore.Models.Api.Responses
+{
+	internal readonly struct GlobalStateResponseDto
+	{
+		public bool LaunchWebAppOnStart { get; }
+		public bool ParseEmojis { get; }
+
+		public GlobalStateResponseDto(GlobalConfig globalConfig)
+		{
+			LaunchWebAppOnStart = globalConfig.LaunchWebAppOnStartup;
+			ParseEmojis = globalConfig.HandleEmojis;
+		}
+	}
+}
