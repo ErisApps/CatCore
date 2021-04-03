b4c867ed918f0bfa7e33b54ab2b8636e51749671
Eris <erisapps@outlook.be>
2021-04-03 16:42:57 +0200

    Added endpoint to request the current Twitch settings state

---
+++ CatCore/Models/Api/Responses/TwitchStateResponseDto.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatCore.Models.Config;
+using CatCore.Models.Twitch.Helix.Responses;
+using CatCore.Models.Twitch.OAuth;
+
+namespace CatCore.Models.Api.Responses
+{
+	internal readonly struct TwitchStateResponseDto
+	{
+		public bool LoggedIn { get; }
+		public bool OwnChannelEnabled { get; }
+
+		public List<TwitchChannelData> ChannelData { get; }
+
+		public bool ParseBttvEmotes { get; }
+		public bool ParseFfzEmotes { get; }
+		public bool ParseTwitchEmotes { get; }
+		public bool ParseCheermotes { get; }
+
+
+		public TwitchStateResponseDto(bool isValid, ValidationResponse? loggedInUser, List<UserData> channelData, TwitchConfig twitchConfig)
+		{
+			LoggedIn = isValid;
+
+			OwnChannelEnabled = twitchConfig.OwnChannelEnabled;
+			ChannelData  = channelData
+				.Select(x => new TwitchChannelData(x.ProfileImageUrl, x.DisplayName, x.LoginName, x.UserId, x.LoginName == loggedInUser?.LoginName))
+				.OrderByDescending(channel => channel.IsSelf)
+				.ThenBy(channel => channel.DisplayName)
+				.ToList();
+
+			ParseBttvEmotes = twitchConfig.ParseBttvEmotes;
+			ParseFfzEmotes = twitchConfig.ParseFfzEmotes;
+			ParseTwitchEmotes = twitchConfig.ParseTwitchEmotes;
+			ParseCheermotes = twitchConfig.ParseCheermotes;
+		}
+	}
+}
