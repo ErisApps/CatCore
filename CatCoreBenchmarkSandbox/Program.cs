9f2c10504b76cd4a5eb75617e6701a0c9dad2572
Eris <erisapps@outlook.be>
2021-04-05 19:14:49 +0200

    Added benchmark for alternative to regex-based IRC message parsing

---
+++ CatCoreBenchmarkSandbox/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using CatCoreBenchmarkSandbox.Benchmarks;
+
+namespace CatCoreBenchmarkSandbox
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<TwitchIrcMessageDeconstructionBenchmark>();
+		}
+	}
+}
