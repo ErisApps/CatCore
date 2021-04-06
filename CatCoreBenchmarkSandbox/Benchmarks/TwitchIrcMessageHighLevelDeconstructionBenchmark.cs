bee980c52fdb6b2dd027ffbe26b71fc5897ddbb1
Eris <erisapps@outlook.be>
2021-04-06 03:37:36 +0200

    Reworked span-based deconstruction logic
    Also added more sample messages

--- CatCoreBenchmarkSandbox/Benchmarks/TwitchIrcMessageHighLevelDeconstructionBenchmark.cs
+++ CatCoreBenchmarkSandbox/Benchmarks/TwitchIrcMessageHighLevelDeconstructionBenchmark.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text.RegularExpressions;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Environments;
@@ -11,13 +13,14 @@ namespace CatCoreBenchmarkSandbox.Benchmarks
 	[MediumRunJob(RuntimeMoniker.Net472, Jit.LegacyJit, Platform.X64)]
 	[Orderer(SummaryOrderPolicy.FastestToSlowest)]
 	[RankColumn(NumeralSystem.Stars)]
+	[MemoryDiagnoser]
 	[CategoriesColumn, AllStatisticsColumn, BaselineColumn, MinColumn, Q1Column, MeanColumn, Q3Column, MaxColumn, MedianColumn]
 	public class TwitchIrcMessageHighLevelDeconstructionBenchmark
 	{
 		private readonly Regex _twitchMessageRegex =
 			new Regex(
 				@"^(?:@(?<Tags>[^\r\n ]*) +|())(?::(?<HostName>[^\r\n ]+) +|())(?<MessageType>[^\r\n ]+)(?: +(?<ChannelName>[^:\r\n ]+[^\r\n ]*(?: +[^:\r\n ]+[^\r\n ]*)*)|())?(?: +:(?<Message>[^\r\n]*)| +())?[\r\n]*$",
-				RegexOptions.Compiled | RegexOptions.Multiline);
+				RegexOptions.Compiled);
 
 		[Params(
 			":tmi.twitch.tv 376 realeris :>",
@@ -25,33 +28,46 @@ namespace CatCoreBenchmarkSandbox.Benchmarks
 			":realeris.tmi.twitch.tv 353 realeris = #realeris :realeris",
 			":realeris.tmi.twitch.tv 366 realeris #realeris :End of /NAMES list",
 			":tmi.twitch.tv CAP * ACK :twitch.tv/tags twitch.tv/commands twitch.tv/membership",
-			"@badge-info=subscriber/1;badges=broadcaster/1,subscriber/0;client-nonce=1ef9899702c12a2081fa33899d7e8465;color=#FF69B4;display-name=RealEris;emotes=;flags=;id=b4595e1c-dd1b-4e45-b7df-a3403c945ad6;mod=0;room-id=405499635;subscriber=1;tmi-sent-ts=1614390981294;turbo=0;user-id=405499635;user-type= :realeris!realeris@realeris.tmi.twitch.tv PRIVMSG #realeris :Heya")]
+			"@badge-info=subscriber/1;badges=broadcaster/1,subscriber/0;client-nonce=1ef9899702c12a2081fa33899d7e8465;color=#FF69B4;display-name=RealEris;emotes=;flags=;id=b4595e1c-dd1b-4e45-b7df-a3403c945ad6;mod=0;room-id=405499635;subscriber=1;tmi-sent-ts=1614390981294;turbo=0;user-id=405499635;user-type= :realeris!realeris@realeris.tmi.twitch.tv PRIVMSG #realeris :Heya",
+			"@badge-info=founder/13;badges=moderator/1,founder/0,bits/1000;client-nonce=05e5fe0b80aadc4c5035303b99d6762a;color=#DAA520;display-name=Scarapter;emotes=;flags=;id=7317d5aa-38ae-4191-88d7-d4d54a3c27bc;mod=1;room-id=62975335;subscriber=0;tmi-sent-ts=1617644034348;turbo=0;user-id=51591450;user-type=mod :scarapter!scarapter@scarapter.tmi.twitch.tv PRIVMSG #lnterz :i definitely dont miss the mass of random charging ports that existed",
+			"@badge-info=;badges=;color=;display-name=bonkeybob;emotes=;flags=;id=108b80a1-7829-4879-86cc-953c3a6b122b;mod=0;room-id=62975335;subscriber=0;tmi-sent-ts=1617644112658;turbo=0;user-id=549616012;user-type= :bonkeybob!bonkeybob@bonkeybob.tmi.twitch.tv PRIVMSG #lnterz :The phrase âitâs just a gameâ is such a weak mindset. You are ok with what happened, losing, imperfection of a craft. When you stop getting angry after losing, youâve lost twice.   Thereâs always something to learn, and always room for improvement, never settle.")]
 		public string IrcMessage;
 
 		[Benchmark(Baseline = true)]
 		public void RegexBenchmark()
 		{
 			var match = _twitchMessageRegex.Match(IrcMessage);
+
+			var tags = match.Groups["Tags"].Success ? match.Groups["Tags"].Value : null;
+			var userName = match.Groups["HostName"].Success ? match.Groups["HostName"].Value.Split('!')[0] : null;
+			var messageType = match.Groups["MessageType"].Value;
+			var channelName = match.Groups["ChannelName"].Success ? match.Groups["ChannelName"].Value.Trim('#') : null;
+			var message = match.Groups["Message"].Success ? match.Groups["Message"].Value : null;
 		}
 
 		[Benchmark]
+		// ReSharper disable once CognitiveComplexity
 		public void SpanDissectionBenchmark()
 		{
 			// Twitch IRC Message spec
 			// https://ircv3.net/specs/extensions/message-tags
 
-			string tags = null;
-			string hostname = null;
-			string messageType = null;
-			string channelName = null;
-			string message = null;
+			// Null-ing this here as I can't do that in the method signature
+			string? tags = null;
+			string? prefix = null;
+			string commandType;
+			string? channelName = null;
+			string? message = null;
+
+			// Twitch IRC Message spec
+			// https://ircv3.net/specs/extensions/message-tags
 
 			var position = 0;
 			var nextSpacePosition = 0;
 
 			var messageAsSpan = IrcMessage.AsSpan();
 
-			void SkipToNextNonSpaceCharacter(in ReadOnlySpan<char> msg)
+			void SkipToNextNonSpaceCharacter(ref ReadOnlySpan<char> msg)
 			{
 				while (position < msg.Length && msg[position] == ' ')
 				{
@@ -71,13 +87,13 @@ namespace CatCoreBenchmarkSandbox.Benchmarks
 				tags = messageAsSpan.Slice(1, nextSpacePosition - 1).ToString();
 
 				position = nextSpacePosition + 1;
-				SkipToNextNonSpaceCharacter(messageAsSpan);
+				SkipToNextNonSpaceCharacter(ref messageAsSpan);
 				messageAsSpan = messageAsSpan.Slice(position);
 				position = 0;
 			}
 
 
-			// Handle hostname
+			// Handle prefix
 			if (messageAsSpan[position] == ':')
 			{
 				nextSpacePosition = messageAsSpan.IndexOf(' ');
@@ -86,10 +102,10 @@ namespace CatCoreBenchmarkSandbox.Benchmarks
 					throw new Exception("Invalid IRC Message");
 				}
 
-				hostname = messageAsSpan.Slice(1, (nextSpacePosition) - 1).ToString();
+				prefix = messageAsSpan.Slice(1, (nextSpacePosition) - 1).ToString();
 
 				position = nextSpacePosition + 1;
-				SkipToNextNonSpaceCharacter(messageAsSpan);
+				SkipToNextNonSpaceCharacter(ref messageAsSpan);
 				messageAsSpan = messageAsSpan.Slice(position);
 				position = 0;
 			}
@@ -101,36 +117,45 @@ namespace CatCoreBenchmarkSandbox.Benchmarks
 			{
 				if (messageAsSpan.Length > position)
 				{
-					messageType = messageAsSpan.ToString();
+					commandType = messageAsSpan.ToString();
+					return;
 				}
 			}
 
-			messageType = messageAsSpan.Slice(0, nextSpacePosition).ToString();
+			commandType = messageAsSpan.Slice(0, nextSpacePosition).ToString();
+
 			position = nextSpacePosition + 1;
-			SkipToNextNonSpaceCharacter(messageAsSpan);
+			SkipToNextNonSpaceCharacter(ref messageAsSpan);
 			messageAsSpan = messageAsSpan.Slice(position);
 			position = 0;
 
-			// Handle ChannelName
-			nextSpacePosition = messageAsSpan.IndexOf(' ');
-			if (nextSpacePosition == -1)
+
+			// Handle channelname and message
+			var handledInLoop = false;
+			while (position < messageAsSpan.Length)
 			{
-				if (messageAsSpan.Length > position)
+				if (messageAsSpan[position] == ':')
 				{
-					channelName = messageAsSpan.ToString();
+					handledInLoop = true;
+
+					// Handle message (extracting this first as we're going to do a lookback in order to determine the previous part)
+					message = messageAsSpan.Slice(position + 1).ToString();
+
+					// Handle everything before the colon as the channelname parameter
+					while (--position < messageAsSpan.Length && messageAsSpan[position] == ' ')
+					{
+					}
+
+					channelName = messageAsSpan.Slice(0, position + 1).ToString();
+					break;
 				}
+
+				position++;
 			}
 
-			channelName = messageAsSpan.Slice(0, nextSpacePosition).ToString();
-			position = nextSpacePosition + 1;
-			SkipToNextNonSpaceCharacter(messageAsSpan);
-			messageAsSpan = messageAsSpan.Slice(position);
-			position = 0;
-
-			// Handle Message
-			if (messageAsSpan[position] == ':')
+			if (!handledInLoop)
 			{
-				message = messageAsSpan.Slice(1).ToString();
+				channelName = messageAsSpan.ToString();
 			}
 		}
 	}
