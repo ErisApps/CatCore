ca7eed6b7295d4737b032f45630dbe74d89d2cdd
Eris <erisapps@outlook.be>
2021-04-15 21:44:39 +0200

    Added interface for platform service managers

---
+++ CatCore/Services/Interfaces/IKittenPlatformServiceManagerBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+namespace CatCore.Services.Interfaces
+{
+	internal interface IKittenPlatformServiceManagerBase : IDisposable
+	{
+		bool IsRunning { get; }
+		void Start(Assembly callingAssembly);
+		void Stop(Assembly? callingAssembly);
+	}
+}
