a8e99a7098941b64045ecb82d28fdd12159c2761
Eris <erisapps@outlook.be>
2021-05-22 00:24:19 +0200

    Initial rate-conforming rate-limiting message sending logic
    Conforms to both broacaster/moderator and normal viewer rate limits.
    Uses sliding-window rate limiting with an internal time window of 32 seconds.

---
+++ CatCore/Models/Twitch/IRC/MessageSendingRateLimit.cs
@@ -0,0 +1,15 @@
+﻿namespace CatCore.Models.Twitch.IRC
+{
+	public enum MessageSendingRateLimit
+	{
+		/// <summary>
+		/// Applies to everyone who doesn't have broadcaster/moderator permissions in the channel to which the message will be send
+		/// </summary>
+		Normal = 20,
+
+		/// <summary>
+		/// Applies to the broadcaster and moderators of the channel to which the message will be send
+		/// </summary>
+		Relaxed = 100
+	}
+}
