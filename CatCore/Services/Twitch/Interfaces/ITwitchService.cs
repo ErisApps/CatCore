25f91db146999c5fc28065bb8ebffff26cfc1dfe
Eris <erisapps@outlook.be>
2021-05-25 03:13:34 +0200

    Exposed more Twitch services

--- CatCore/Services/Twitch/Interfaces/ITwitchService.cs
+++ CatCore/Services/Twitch/Interfaces/ITwitchService.cs
@@ -1,11 +1,23 @@
 ﻿using CatCore.Services.Interfaces;
+using JetBrains.Annotations;
 
 namespace CatCore.Services.Twitch.Interfaces
 {
 	public interface ITwitchService : IPlatformService
 	{
+		[PublicAPI]
 		ITwitchPubSubServiceManager GetPubSubService();
+
+		[PublicAPI]
 		ITwitchHelixApiService GetHelixApiService();
+
+		[PublicAPI]
 		ITwitchRoomStateTrackerService GetRoomStateTrackerService();
+
+		[PublicAPI]
+		ITwitchUserStateTrackerService GetUserStateTrackerService();
+
+		[PublicAPI]
+		ITwitchChannelManagementService GetChannelManagementService();
 	}
 }
