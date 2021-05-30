e4c3ae353d47454448180e26d2b0d1b944447931
Eris <erisapps@outlook.be>
2021-05-30 20:31:39 +0200

    Added CatCore.Azure project
    An OAuth authentication back-end to securely authorize with various platforms, powered by Azure functions

---
+++ CatCore.Azure/Functions/Twitch/RefreshTokensFunction.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using CatCore.Azure.Services.Twitch;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CatCore.Azure.Functions.Twitch
+{
+	public class RefreshTokensFunction
+	{
+		[Function("RefreshTokensFunction")]
+		public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "twitch/refresh")]
+			HttpRequestData req,
+			FunctionContext executionContext)
+		{
+			var logger = executionContext.GetLogger(nameof(RefreshTokensFunction));
+
+			var queryParametersCollection = HttpUtility.ParseQueryString(req.Url.Query);
+			var refreshToken = queryParametersCollection["refresh_token"];
+			if (string.IsNullOrWhiteSpace(refreshToken))
+			{
+				logger.LogWarning("API Call is missing one of the required query parameters");
+				return req.CreateResponse(HttpStatusCode.BadRequest);
+			}
+
+			var twitchAuthService = executionContext.InstanceServices.GetService<TwitchAuthService>()!;
+			var authorizationResponse = await twitchAuthService.RefreshTokens(refreshToken).ConfigureAwait(false);
+
+			HttpResponseData response;
+			if (authorizationResponse != null)
+			{
+				response = req.CreateResponse(HttpStatusCode.OK);
+				await response.WriteAsJsonAsync(authorizationResponse).ConfigureAwait(false);
+			}
+			else
+			{
+				logger.LogInformation("Couldn't refresh existing credentials through Twitch Auth server");
+				response = req.CreateResponse(HttpStatusCode.Unauthorized);
+			}
+
+			return response;
+		}
+	}
+}
---
+++ CatCore.Azure/Services/Twitch/TwitchAuthService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using CatCore.Shared.Models.Twitch.OAuth;
+using Microsoft.Extensions.Logging;
+
+namespace CatCore.Azure.Services.Twitch
+{
+	internal class TwitchAuthService
+	{
+		private const string TWITCH_AUTH_BASEURL = "https://id.twitch.tv/oauth2/";
+
+		private readonly ILogger<TwitchAuthService> _logger;
+		private readonly HttpClient _authClient;
+
+		public TwitchAuthService(ILogger<TwitchAuthService> logger, IHttpClientFactory httpClientFactory)
+		{
+			_logger = logger;
+
+			_authClient = httpClientFactory.CreateClient();
+			_authClient.DefaultRequestVersion = HttpVersion.Version20;
+			_authClient.BaseAddress = new Uri(TWITCH_AUTH_BASEURL, UriKind.Absolute);
+			_authClient.DefaultRequestHeaders.UserAgent.TryParseAdd($"{nameof(CatCore)}/1.0.0");
+		}
+
+		public async Task<AuthorizationResponse?> GetTokensByAuthorizationCode(string authorizationCode, string redirectUrl)
+		{
+			var responseMessage = await _authClient
+				.PostAsync($"{TWITCH_AUTH_BASEURL}token" +
+				           $"?client_id={Environment.GetEnvironmentVariable("Twitch_CatCore_ClientId")}" +
+				           $"&client_secret={Environment.GetEnvironmentVariable("Twitch_CatCore_ClientSecret")}" +
+				           $"&code={authorizationCode}" +
+				           "&grant_type=authorization_code" +
+				           $"&redirect_uri={redirectUrl}", null!)
+				.ConfigureAwait(false);
+
+			if (!responseMessage.IsSuccessStatusCode)
+			{
+				return null;
+			}
+
+			return await responseMessage.Content.ReadFromJsonAsync<AuthorizationResponse?>().ConfigureAwait(false);
+		}
+
+		public async Task<AuthorizationResponse?> RefreshTokens(string refreshToken)
+		{
+			if (string.IsNullOrWhiteSpace(refreshToken))
+			{
+				return null;
+			}
+
+			var responseMessage = await _authClient
+				.PostAsync($"{TWITCH_AUTH_BASEURL}token" +
+				           $"?client_id={Environment.GetEnvironmentVariable("Twitch_CatCore_ClientId")}" +
+				           $"&client_secret={Environment.GetEnvironmentVariable("Twitch_CatCore_ClientSecret")}" +
+				           "&grant_type=refresh_token" +
+				           $"&refresh_token={refreshToken}", null!)
+				.ConfigureAwait(false);
+
+			if (!responseMessage.IsSuccessStatusCode)
+			{
+				return null;
+			}
+
+			return await responseMessage.Content.ReadFromJsonAsync<AuthorizationResponse?>().ConfigureAwait(false); ;
+		}
+	}
+}
