5b678de700dde46f85648f102cfeba43f259e71b
Eris <erisapps@outlook.be>
2021-06-19 22:52:15 +0200

    Added StartsWith benchmark
    Just a test case to see whether it's faster to use StartsWith on string level or do the conversion to Span<T>

---
+++ CatCoreBenchmarkSandbox/Benchmarks/Miscellaneous/StringStartsWithBenchmark.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Order;
+
+namespace CatCoreBenchmarkSandbox.Benchmarks.Miscellaneous
+{
+	[MediumRunJob(RuntimeMoniker.Net472, Jit.LegacyJit, Platform.X64)]
+	[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+	[RankColumn(NumeralSystem.Stars)]
+	[MemoryDiagnoser]
+	[CategoriesColumn, AllStatisticsColumn, BaselineColumn, MinColumn, Q1Column, MeanColumn, Q3Column, MaxColumn, MedianColumn]
+	public class StringStartsWithBenchmark
+	{
+		[Params(
+			"ACTION Heya",
+			"ACTION i definitely dont miss the mass of random charging ports that existed",
+			"ACTION The phrase âitâs just a gameâ is such a weak mindset. You are ok with what happened, losing, imperfection of a craft. When you stop getting angry after losing, youâve lost twice.   Thereâs always something to learn, and always room for improvement, never settle.",
+			"ACTION Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p")]
+		public string Message;
+
+		[Benchmark(Baseline = true)]
+		public bool StringStartsWith()
+		{
+			const string ACTION = "ACTION ";
+			return Message.StartsWith(ACTION, StringComparison.Ordinal);
+		}
+
+		[Benchmark]
+		public bool SpanStartsWith()
+		{
+			const string ACTION = "ACTION ";
+			return Message.AsSpan().StartsWith(ACTION.AsSpan(), StringComparison.Ordinal);
+		}
+	}
+}
