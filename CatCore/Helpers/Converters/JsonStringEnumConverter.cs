1a8eb6baade4c1d26f01147d493cda695f4b4c6b
Eris <erisapps@outlook.be>
2021-07-18 05:49:31 +0200

    Added End Poll endpoint to Twitch Helix service

---
+++ CatCore/Helpers/Converters/JsonStringEnumConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CatCore.Helpers.Converters
+{
+	/// <remarks>
+	/// Based upon https://github.com/dotnet/runtime/issues/31081#issuecomment-848697673
+	/// </remarks>
+	internal class JsonStringEnumConverter<TEnum> : JsonConverter<TEnum> where TEnum : struct, Enum
+	{
+		private readonly Dictionary<TEnum, string> _enumToString = new();
+		private readonly Dictionary<string, TEnum> _stringToEnum = new();
+
+		public JsonStringEnumConverter()
+		{
+			var type = typeof(TEnum);
+			var values = Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToList();
+
+			foreach (var value in values)
+			{
+				var enumMember = type.GetMember(value.ToString())[0];
+				var attr = enumMember.GetCustomAttributes(typeof(EnumMemberAttribute), false)
+					.Cast<EnumMemberAttribute>()
+					.FirstOrDefault();
+
+				_stringToEnum.Add(value.ToString(), value);
+
+				if (attr?.Value != null)
+				{
+					_enumToString.Add(value, attr.Value);
+					_stringToEnum.Add(attr.Value, value);
+				}
+				else
+				{
+					_enumToString.Add(value, value.ToString());
+				}
+			}
+		}
+
+		public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			var stringValue = reader.GetString();
+
+			if (stringValue != null && _stringToEnum.TryGetValue(stringValue, out var enumValue))
+			{
+				return enumValue;
+			}
+
+			return default;
+		}
+
+		public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
+		{
+			writer.WriteStringValue(_enumToString[value]);
+		}
+	}
+}
---
+++ CatCore/Models/Twitch/Helix/Requests/Polls/EndPollRequestDto.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using CatCore.Helpers.Converters;
+using CatCore.Models.Twitch.Helix.Shared;
+
+namespace CatCore.Models.Twitch.Helix.Requests.Polls
+{
+	internal readonly struct EndPollRequestDto
+	{
+		[JsonPropertyName("broadcaster_id")]
+		public string BroadcasterId { get; }
+
+		[JsonPropertyName("id")]
+		public string PollId { get; }
+
+		[JsonPropertyName("status")]
+		[JsonConverter(typeof(JsonStringEnumConverter<PollStatus>))]
+		public PollStatus Status { get; }
+
+		public EndPollRequestDto(string broadcasterId, string pollId, PollStatus status)
+		{
+			BroadcasterId = broadcasterId;
+			PollId = pollId;
+			Status = status;
+		}
+	}
+}
