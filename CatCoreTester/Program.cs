f8595b46f9b3931aa837a5c00cf70e64d4e6216c
Eris <erisapps@outlook.be>
2021-09-11 15:05:06 +0200

    Add some more testing code

--- CatCoreTester/Program.cs
+++ CatCoreTester/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using CatCore;
-using CatCore.Services.Twitch.Interfaces;
-using DryIoc;
+using CatCore.Services.Twitch;
 
 namespace CatCoreTester
 {
@@ -17,19 +15,108 @@ namespace CatCoreTester
 			stoppyWatch.Start();
 			var chatCoreInstance = ChatCoreInstance.CreateInstance(
 #if !DEBUG
-				(level, context, message) => Console.Write($"External logger: {level}|{context}|{message}")
+				(level, context, message) => Console.Write($"External logger: {DateTimeOffset.Now:O}|{level}|{context}|{message}")
 #endif
 			);
 
 			stoppyWatch.Stop();
 
-			Console.WriteLine($"Tester finished. Instance creation time: {stoppyWatch.Elapsed:g}");
+			Console.WriteLine($"Tester finished. Instance creation time: {(double)stoppyWatch.ElapsedTicks/TimeSpan.TicksPerMillisecond}ms");
 			Console.WriteLine();
 			Console.WriteLine("========================");
 			Console.WriteLine("WARNING: Test below will only work in DEBUG mode due to the IoC container being exposed only in that build mode.");
 			Console.WriteLine("WARNING: It also currently relies on in-memory access and refresh tokens in the TwitchAuthService, so make sure to login first before continue-ing.");
 			Console.WriteLine();
 			Console.WriteLine("Press \"c\" to continue...");
+
+			chatCoreInstance.LaunchWebPortal();
+
+			while (Console.ReadKey().KeyChar != 'c')
+			{
+				await Task.Delay(TimeSpan.FromMilliseconds(100)).ConfigureAwait(false);
+			}
+
+			var chatServiceMultiplexer = chatCoreInstance.RunAllServices();
+			chatServiceMultiplexer.OnLogin += async service =>
+			{
+				await Console.Out.WriteLineAsync("Logged in... presumably").ConfigureAwait(false);
+				if (service is TwitchService twitchService)
+				{
+					var helix = twitchService.GetHelixApiService();
+
+					/*var pollHistory = await helix.GetPolls().ConfigureAwait(false);
+
+					var pollData = await helix.CreatePoll("Is this a CatCore generated poll?", new List<string> {"Yes!!!", "No?"}, 60, channelPointsVotingEnabled: true, channelPointsPerVote: 15).ConfigureAwait(false);
+					// var pollData = await helix.CreatePoll("CatCore: Should Eris stay awake?", new List<string> {"Yes!!!", "No?"}, 60).ConfigureAwait(false);
+					var first = pollData.Value.Data.First();
+					var firstStatus = first.Status;
+					var firstStartedAt = first.StartedAt;
+
+
+					var terminatedResult = await helix.EndPoll(first.Id, PollStatus.Terminated).ConfigureAwait(false);*/
+
+
+					/*var predictionHistory = await helix.GetPredictions().ConfigureAwait(false);
+					var lastPrediction = predictionHistory.Value.Data.First();
+					if (lastPrediction.Status == PredictionStatus.Active || lastPrediction.Status == PredictionStatus.Locked)
+					{
+						await helix.EndPrediction(lastPrediction.Id, PredictionStatus.Cancelled);
+					}
+
+					var predictionData = await helix.CreatePrediction("CatCore: Will someone vote on this?", new List<string> {"Yes!!!", "No?"}, 60).ConfigureAwait(false);
+					var first = predictionData.Value.Data.First();*/
+
+
+					// await helix.EndPrediction(first.Id, PredictionStatus.Cancelled).ConfigureAwait(false);
+
+					Console.WriteLine("Can't touch this.");
+				}
+			};
+
+
+
+			/*
+
+			var sema = new SemaphoreSlim(1, 1);
+
+			async void OnChatServiceMultiplexerOnOnJoinChannel(IPlatformService service, IChatChannel channel)
+			{
+				if (channel.Id != twitchChannel.Id)
+				{
+					return;
+				}
+
+				if (!await sema.WaitAsync(0).ConfigureAwait(false))
+				{
+					return;
+				}
+
+				/*await Task.Delay(TimeSpan.FromSeconds(35)).ConfigureAwait(false);
+
+				for (var i = 0; i < 10; i++)
+				{
+					chatServiceMultiplexer.SendMessage(channel, "!bomb");
+
+					await Task.Delay(TimeSpan.FromSeconds(35)).ConfigureAwait(false);
+				}#1#
+
+				/*const int count = 1;
+
+				chatServiceMultiplexer.SendMessage(twitchChannel, $"Testing being conform with rate-limit. Message -01. Initial message... {count} msg burst will start in roughly 10 seconds from now.");
+
+				await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+
+				for (var i = 0; i < count; i++)
+				{
+					chatServiceMultiplexer.SendMessage(twitchChannel, $"Testing being conform with rate-limit. Message {i + 1:000}");
+				}#1#
+			}
+
+			chatServiceMultiplexer.OnJoinChannel += OnChatServiceMultiplexerOnOnJoinChannel;
+			*/
+
+			// await Task.Delay(5000).ConfigureAwait(false);
+			// chatCoreInstance.StopTwitchServices();
 
 			while (Console.ReadKey().KeyChar != 'c')
 			{
