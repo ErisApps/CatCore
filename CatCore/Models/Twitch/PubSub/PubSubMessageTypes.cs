f43410adeed0df49a85a5f8ab49d9ed5a14ccf27
Eris <erisapps@outlook.be>
2021-09-15 18:12:37 +0200

    Added some PubSub helper classes

---
+++ CatCore/Models/Twitch/PubSub/PubSubMessageTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace CatCore.Models.Twitch.PubSub
+{
+	internal static class PubSubMessageTypes
+	{
+		public const string RESPONSE = nameof(RESPONSE);
+		public const string PONG = nameof(PONG);
+		public const string RECONNECT = nameof(RECONNECT);
+		public const string MESSAGE = nameof(MESSAGE);
+	}
+}
---
+++ CatCore/Models/Twitch/PubSub/PubSubTopics.cs
@@ -0,0 +1,9 @@
+﻿namespace CatCore.Models.Twitch.PubSub
+{
+	internal static class PubSubTopics
+	{
+		public const string VIDEO_PLAYBACK = "video-playback-by-id";
+
+		public static string FormatVideoPlaybackTopic(string channelId) => VIDEO_PLAYBACK + "." + channelId;
+	}
+}
