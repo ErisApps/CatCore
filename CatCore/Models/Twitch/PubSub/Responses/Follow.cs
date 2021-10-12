c4e9211b9a9c64557889a86e3f2bd4184eb9ab1c
Eris <erisapps@outlook.be>
2021-10-13 01:06:32 +0200

    Implemented Follow PubSub message parsing in experimental agent

---
+++ CatCore/Models/Twitch/PubSub/Responses/Follow.cs
@@ -0,0 +1,16 @@
+﻿namespace CatCore.Models.Twitch.PubSub.Responses
+{
+	public readonly struct Follow
+	{
+		public string UserId { get; }
+		public string Username { get; }
+		public string DisplayName { get; }
+
+		public Follow(string userId, string username, string displayName)
+		{
+			UserId = userId;
+			Username = username;
+			DisplayName = displayName;
+		}
+	}
+}
