611460dd3beec2e60e047779ce4ff37ee1d65353
Eris <erisapps@outlook.be>
2021-10-19 00:39:19 +0200

    Added missing Json serialization attributes to PubSub Follow struct

--- CatCore/Models/Twitch/PubSub/Responses/Follow.cs
+++ CatCore/Models/Twitch/PubSub/Responses/Follow.cs
@@ -1,11 +1,19 @@
-﻿namespace CatCore.Models.Twitch.PubSub.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace CatCore.Models.Twitch.PubSub.Responses
 {
 	public readonly struct Follow
 	{
+		[JsonPropertyName("user_id")]
 		public string UserId { get; }
+
+		[JsonPropertyName("username")]
 		public string Username { get; }
+
+		[JsonPropertyName("display_name")]
 		public string DisplayName { get; }
 
+		[JsonConstructor]
 		public Follow(string userId, string username, string displayName)
 		{
 			UserId = userId;
