595c4ec7866abd23fc129bd9f3e6ccfa7692ec7d
Eris <erisapps@outlook.be>
2021-10-18 23:53:01 +0200

    Added contributor fields to Poll data object

---
+++ CatCore/Models/Twitch/PubSub/Responses/Polls/ContributorBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatCore.Models.Twitch.PubSub.Responses.Polls
+{
+	public abstract class ContributorBase
+	{
+		[JsonPropertyName("user_id")]
+		public string UserId { get; }
+
+		[JsonPropertyName("display_name")]
+		public string DisplayName { get; }
+
+		protected ContributorBase(string userId, string displayName)
+		{
+			UserId = userId;
+			DisplayName = displayName;
+		}
+	}
+}
