6f824cc77f6ff09b1b7bdad2df03d0bb5e1630b7
Eris <erisapps@outlook.be>
2021-10-27 21:27:12 +0200

    Added Timer triggered function to keep functions instance warm

---
+++ CatCore.Azure/Functions/KeepAliveFunction.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace CatCore.Azure.Functions
+{
+	public static class KeepAliveFunction
+	{
+		[Function("KeepAliveFunction")]
+		public static void Run([TimerTrigger("0 */10 * * * *")] TimerInfo timerInfo, FunctionContext context)
+		{
+		}
+	}
+}
