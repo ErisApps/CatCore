f4378e0bd31fbd262af2336f45e9c7b2ca186192
Eris <erisapps@outlook.be>
2021-11-08 00:56:25 +0100

    Merge TopicNegotiationMessage and MessageBase PubSub classes

--- CatCore/Models/Twitch/PubSub/Requests/TopicNegotiationMessage.cs
+++ CatCore/Models/Twitch/PubSub/Requests/TopicNegotiationMessage.cs
@@ -2,16 +2,20 @@
 
 namespace CatCore.Models.Twitch.PubSub.Requests
 {
-	internal sealed class TopicNegotiationMessage : MessageBase
+	internal sealed class TopicNegotiationMessage
 	{
 		internal const string LISTEN = nameof(LISTEN);
 		internal const string UNLISTEN = nameof(UNLISTEN);
 
-		public TopicNegotiationMessage(string type, TopicNegotiationMessageData data, string nonce) : base(type)
+		public TopicNegotiationMessage(string type, TopicNegotiationMessageData data, string nonce)
 		{
+			Type = type;
 			Data = data;
 			Nonce = nonce;
 		}
+
+		[JsonPropertyName("type")]
+		public string Type { get; }
 
 		[JsonPropertyName("data")]
 		public TopicNegotiationMessageData Data { get; }
