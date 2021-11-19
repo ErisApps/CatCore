11d1c0a696997f4dfb80c67bdeae865eec519b7c
Eris <erisapps@outlook.be>
2021-11-20 00:50:32 +0100

    Implemented and deduplicated parsing logic in BttvDataProvider

---
+++ CatCore/Models/Shared/ChatResourceData.cs
@@ -0,0 +1,20 @@
+﻿namespace CatCore.Models.Shared
+{
+	public readonly struct ChatResourceData
+	{
+		public string Id { get; }
+		public string Name { get; }
+		public string Uri { get; }
+		public bool IsAnimated { get; }
+		public string Type { get; }
+
+		public ChatResourceData(string id, string name, string uri, bool isAnimated, string type)
+		{
+			Id = id;
+			Name = name;
+			Uri = uri;
+			IsAnimated = isAnimated;
+			Type = type;
+		}
+	}
+}
