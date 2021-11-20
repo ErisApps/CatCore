e38b455bbc9e3a1a3fb1da1c8a233f6fd4a25b02
Eris <erisapps@outlook.be>
2021-11-20 02:08:32 +0100

    Fixed bug where failed token refreshes would still allow attempts to connect to underlying websockets

--- CatCore/Services/Twitch/TwitchIrcService.cs
+++ CatCore/Services/Twitch/TwitchIrcService.cs
@@ -99,9 +99,9 @@ namespace CatCore.Services.Twitch
 				return;
 			}
 
-			if (!_twitchAuthService.TokenIsValid)
+			if (!(_twitchAuthService.TokenIsValid || await _twitchAuthService.RefreshTokens().ConfigureAwait(false)))
 			{
-				_ = await _twitchAuthService.RefreshTokens().ConfigureAwait(false);
+				return;
 			}
 
 			_kittenWebSocketProvider.ConnectHappened -= ConnectHappenedHandler;
