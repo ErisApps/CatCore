ed2ea5c8ba7f0351237a856a6c2a3b816460aa41
Eris <erisapps@outlook.be>
2021-11-24 20:08:24 +0100

    Add additional benchmark case to EmojiParserBenchmark

--- CatCoreBenchmarkSandbox/Benchmarks/EmojiParser/EmojiParserBenchmark.cs
+++ CatCoreBenchmarkSandbox/Benchmarks/EmojiParser/EmojiParserBenchmark.cs
@@ -20,7 +20,8 @@ namespace CatCoreBenchmarkSandbox.Benchmarks.EmojiParser
 		}
 
 		[Params("I 🧡 Twemoji! 🥳",
-			"I've eaten Chinese food 😱😍🍱🍣🍥🍙🍘🍚🍜🍱🍣🍥🍙🍘🍚🍜")]
+			"I've eaten Chinese food 😱😍🍱🍣🍥🍙🍘🍚🍜🍱🍣🍥🍙🍘🍚🍜",
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")]
 		public string Message = null!;
 
 		[GlobalSetup(Target = nameof(CatCoreTwemojiReimplementationBenchmark))]
