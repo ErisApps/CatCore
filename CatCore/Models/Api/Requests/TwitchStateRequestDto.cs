145ec79b0f8616d9304f85c50f7e15bc05bf7f7d
Eris <erisapps@outlook.be>
2021-11-28 23:55:49 +0100

    Fixed some formatting

--- CatCore/Models/Api/Requests/TwitchStateRequestDto.cs
+++ CatCore/Models/Api/Requests/TwitchStateRequestDto.cs
@@ -6,10 +6,12 @@ namespace CatCore.Models.Api.Requests
 	internal readonly struct TwitchStateRequestDto
 	{
 		public bool SelfEnabled { get; }
+
 		/// <remark>
 		/// Key being the actual userId and the value being the loginname
 		/// </remark>
 		public Dictionary<string, string> AdditionalChannelsData { get; }
+
 		public bool ParseBttvEmotes { get; }
 		public bool ParseFfzEmotes { get; }
 		public bool ParseTwitchEmotes { get; }
