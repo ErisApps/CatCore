c824ffcbf84a64a0b11b808480ff26410b9b4480
Eris <erisapps@outlook.be>
2022-02-08 19:55:17 +0100

    Add documentation to TwitchRoomStateTrackerService

--- CatCore/Services/Twitch/Interfaces/ITwitchRoomStateTrackerService.cs
+++ CatCore/Services/Twitch/Interfaces/ITwitchRoomStateTrackerService.cs
@@ -6,6 +6,11 @@ namespace CatCore.Services.Twitch.Interfaces
 {
 	public interface ITwitchRoomStateTrackerService
 	{
+		/// <summary>
+		/// Returns the RoomState for the specified channelName.
+		/// </summary>
+		/// <remarks>This is only available when the channel is successfully joined over IRC</remarks>
+		/// <returns>RoomState for the specified channel</returns>
 		[PublicAPI]
 		TwitchRoomState? GetRoomState(string channelName);
 
