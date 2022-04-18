81d51f1c6bfa4e1f984d4339c0e40bb2849f097b
Eris <erisapps@outlook.be>
2022-04-18 23:32:16 +0200

    Adding AsyncEventHandlerDefinitions

---
+++ CatCore/Helpers/AsyncEventHandlerDefinitions.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace CatCore.Helpers
+{
+	internal static class AsyncEventHandlerDefinitions
+	{
+		public delegate Task AsyncEventHandler();
+		public delegate Task AsyncEventHandler<T1>(T1 t1);
+		public delegate Task AsyncEventHandler<T1, T2>(T1 t1, T2 t2);
+	}
+}
