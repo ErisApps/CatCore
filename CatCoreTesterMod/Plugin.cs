e2d2fa70a8ab3f46c97f804fd93a87ee7344bdcb
Eris <erisapps@outlook.be>
2022-05-10 19:58:57 +0200

    Improve performance and DynamicInit support of CatCore Tester mod

--- CatCoreTesterMod/Plugin.cs
+++ CatCoreTesterMod/Plugin.cs
@@ -8,21 +8,42 @@ namespace CatCoreTesterMod
 	[Plugin(RuntimeOptions.DynamicInit), NoEnableDisable]
 	internal class Plugin
 	{
+		private readonly Logger _catCoreLogger;
+
+		private readonly CatCoreInstance _catCoreInstance;
+
 		[Init]
 		public Plugin(Logger logger)
 		{
-			CatCoreInstance.Create((level, context, message) => logger
-				.GetChildLogger("CatCore")
-				.Log(level switch
-				{
-					CustomLogLevel.Trace => Logger.Level.Trace,
-					CustomLogLevel.Debug => Logger.Level.Debug,
-					CustomLogLevel.Information => Logger.Level.Info,
-					CustomLogLevel.Warning => Logger.Level.Warning,
-					CustomLogLevel.Error => Logger.Level.Error,
-					CustomLogLevel.Critical => Logger.Level.Critical,
-					_ => Logger.Level.Debug
-				}, $"{context} | {message}"));
+			_catCoreLogger = logger.GetChildLogger(nameof(CatCore));
+			_catCoreInstance = CatCoreInstance.Create(CatCoreOnLogReceived);
+		}
+
+		[OnEnable]
+		public void OnEnable()
+		{
+			_catCoreInstance.OnLogReceived -= CatCoreOnLogReceived;
+			_catCoreInstance.OnLogReceived += CatCoreOnLogReceived;
+		}
+
+		[OnDisable]
+		public void OnDisable()
+		{
+			_catCoreInstance.OnLogReceived -= CatCoreOnLogReceived;
+		}
+
+		private void CatCoreOnLogReceived(CustomLogLevel level, string context, string message)
+		{
+			_catCoreLogger.Log(level switch
+			{
+				CustomLogLevel.Trace => Logger.Level.Trace,
+				CustomLogLevel.Debug => Logger.Level.Debug,
+				CustomLogLevel.Information => Logger.Level.Info,
+				CustomLogLevel.Warning => Logger.Level.Warning,
+				CustomLogLevel.Error => Logger.Level.Error,
+				CustomLogLevel.Critical => Logger.Level.Critical,
+				_ => Logger.Level.Debug
+			}, $"{context} | {message}");
 		}
 	}
 }
