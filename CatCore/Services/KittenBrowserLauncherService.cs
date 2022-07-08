5d5834ed284319cee108daba4becf5788c4ac6aa
PixelBoom <33881026+rithik-b@users.noreply.github.com>
2022-07-08 13:00:16 -0400

    Fixed browser launch bug for windows and linux

--- CatCore/Services/KittenBrowserLauncherService.cs
+++ CatCore/Services/KittenBrowserLauncherService.cs
@@ -8,7 +8,21 @@ namespace CatCore.Services
 	{
 		public void LaunchWebPortal()
 		{
-			Task.Run(() => Process.Start(ConstantsBase.InternalApiServerUri));
+			Task.Run(() =>
+			{
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				{
+					Process.Start(ConstantsBase.InternalApiServerUri);
+				}
+				else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				{
+					Process.Start("xdg-open", ConstantsBase.InternalApiServerUri);
+				}
+				else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+				{
+					Process.Start("open", ConstantsBase.InternalApiServerUri);
+				}
+			});
 		}
 	}
 }
