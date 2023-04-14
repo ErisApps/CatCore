403e2dbaeb408926353dfd918691fa32d67228cc
Eris <erisapps@outlook.be>
2023-04-14 16:00:27 +0200

    Redo usings in TwitchHelixSerializerContext

--- CatCore/Helpers/JSON/TwitchHelixSerializerContext.cs
+++ CatCore/Helpers/JSON/TwitchHelixSerializerContext.cs
@@ -1,49 +1,38 @@
 using System.Text.Json.Serialization;
-using CatCore.Models.Twitch.Helix.Requests;
-using CatCore.Models.Twitch.Helix.Requests.Bans;
-using CatCore.Models.Twitch.Helix.Requests.Polls;
-using CatCore.Models.Twitch.Helix.Requests.Predictions;
-using CatCore.Models.Twitch.Helix.Responses;
-using CatCore.Models.Twitch.Helix.Responses.Badges;
-using CatCore.Models.Twitch.Helix.Responses.Bans;
-using CatCore.Models.Twitch.Helix.Responses.Bits.Cheermotes;
-using CatCore.Models.Twitch.Helix.Responses.Emotes;
-using CatCore.Models.Twitch.Helix.Responses.Polls;
-using CatCore.Models.Twitch.Helix.Responses.Predictions;
 using HelixRequests = CatCore.Models.Twitch.Helix.Requests;
 using HelixResponses = CatCore.Models.Twitch.Helix.Responses;
 
 namespace CatCore.Helpers.JSON
 {
-	[JsonSerializable(typeof(ResponseBase<UserData>))]
-	[JsonSerializable(typeof(CreateStreamMarkerRequestDto))]
-	[JsonSerializable(typeof(ResponseBase<CreateStreamMarkerData>))]
-	[JsonSerializable(typeof(ResponseBaseWithPagination<ChannelData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.UserData>))]
+	[JsonSerializable(typeof(HelixRequests.CreateStreamMarkerRequestDto))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.CreateStreamMarkerData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBaseWithPagination<HelixResponses.ChannelData>))]
 	[JsonSerializable(typeof(HelixRequests.Polls.PollChoice), TypeInfoPropertyName = "RequestPollChoice")]
 	[JsonSerializable(typeof(HelixResponses.Polls.PollChoice), TypeInfoPropertyName = "ResponsePollChoice")]
-	[JsonSerializable(typeof(ResponseBaseWithPagination<PollData>))]
-	[JsonSerializable(typeof(CreatePollRequestDto))]
-	[JsonSerializable(typeof(EndPollRequestDto))]
-	[JsonSerializable(typeof(ResponseBase<PollData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBaseWithPagination<HelixResponses.Polls.PollData>))]
+	[JsonSerializable(typeof(HelixRequests.Polls.CreatePollRequestDto))]
+	[JsonSerializable(typeof(HelixRequests.Polls.EndPollRequestDto))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.Polls.PollData>))]
 	[JsonSerializable(typeof(HelixRequests.Predictions.Outcome), TypeInfoPropertyName = "RequestOutcome")]
 	[JsonSerializable(typeof(HelixResponses.Predictions.Outcome), TypeInfoPropertyName = "ResponseOutcome")]
-	[JsonSerializable(typeof(ResponseBaseWithPagination<PredictionData>))]
-	[JsonSerializable(typeof(CreatePredictionsRequestDto))]
-	[JsonSerializable(typeof(EndPredictionRequestDto))]
-	[JsonSerializable(typeof(ResponseBase<PredictionData>))]
-	[JsonSerializable(typeof(ResponseBase<CheermoteGroupData>))]
-	[JsonSerializable(typeof(ResponseBase<BadgeData>))]
-	[JsonSerializable(typeof(ResponseBaseWithPagination<Stream>))]
-	[JsonSerializable(typeof(ResponseBaseWithTemplate<GlobalEmote>))]
-	[JsonSerializable(typeof(ResponseBaseWithTemplate<ChannelEmote>))]
-	[JsonSerializable(typeof(ChatSettingsRequestDto))]
-	[JsonSerializable(typeof(ResponseBase<ChatSettings>))]
-	[JsonSerializable(typeof(ResponseBaseWithPagination<BannedUserInfo>))]
-	[JsonSerializable(typeof(LegacyRequestDataWrapper<BanUserRequestDto>))]
-	[JsonSerializable(typeof(ResponseBase<BanUser>))]
-	[JsonSerializable(typeof(SendChatAnnouncementRequestDto))]
-	[JsonSerializable(typeof(ResponseBase<UserChatColorData>))]
-	[JsonSerializable(typeof(ResponseBase<StartRaidData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBaseWithPagination<HelixResponses.Predictions.PredictionData>))]
+	[JsonSerializable(typeof(HelixRequests.Predictions.CreatePredictionsRequestDto))]
+	[JsonSerializable(typeof(HelixRequests.Predictions.EndPredictionRequestDto))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.Predictions.PredictionData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.Bits.Cheermotes.CheermoteGroupData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.Badges.BadgeData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBaseWithPagination<HelixResponses.Stream>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBaseWithTemplate<HelixResponses.Emotes.GlobalEmote>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBaseWithTemplate<HelixResponses.Emotes.ChannelEmote>))]
+	[JsonSerializable(typeof(HelixRequests.ChatSettingsRequestDto))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.ChatSettings>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBaseWithPagination<HelixResponses.Bans.BannedUserInfo>))]
+	[JsonSerializable(typeof(HelixRequests.LegacyRequestDataWrapper<HelixRequests.Bans.BanUserRequestDto>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.Bans.BanUser>))]
+	[JsonSerializable(typeof(HelixRequests.SendChatAnnouncementRequestDto))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.UserChatColorData>))]
+	[JsonSerializable(typeof(HelixResponses.ResponseBase<HelixResponses.StartRaidData>))]
 	internal partial class TwitchHelixSerializerContext : JsonSerializerContext
 	{
 	}
